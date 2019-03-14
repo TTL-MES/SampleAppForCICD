f8f7b9b84125f21732802442c01101a859d11b39
VilasSuryawanshi <48517770+VilasSuryawanshi@users.noreply.github.com>
2019-03-14 07:07:18 -0700

    Update UnitTest1.cs

--- SampleApplication.Test/UnitTest1.cs
+++ SampleApplication.Test/UnitTest1.cs
@@ -4,15 +4,27 @@ using SampleApplication;
 
 namespace SampleApplication.Test
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
+   [TestMethod]
         public void TestMethod1()
         {
            int expectedValue = 6;
            int actualValu= BusinessLayer.Add(3, 3);
            Assert.AreEqual(expectedValue, actualValu);
         }
-    }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int expectedValue = 6;
+            int actualValu = BusinessLayer.Add(3, 3);
+            Assert.AreEqual(expectedValue, actualValu);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int expectedValue = 7;
+            int actualValu = BusinessLayer.Add(3, 3);
+            Assert.AreEqual(expectedValue, actualValu);
+        }
 }
